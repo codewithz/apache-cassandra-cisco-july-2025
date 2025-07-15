a5532a8135f025c0bc99ca82daa56b4ff241779b
Zartab Nakhwa <zartab@codewithz.com>
2025-07-15 16:58:15 +0530

    Updated Cluster Values

--- cassandra-playground/src/main/java/com/cisco/App.java
+++ cassandra-playground/src/main/java/com/cisco/App.java
@@ -11,8 +11,8 @@ public class App {
         try (CqlSession session = CqlSession.builder().build()) {
             System.out.println("✅ Connected to Cassandra!");
 
-            session.execute("CREATE KEYSPACE IF NOT EXISTS demo WITH replication = {'class':'SimpleStrategy', 'replication_factor':1};");
-            session.execute("USE demo;");
+            session.execute("CREATE KEYSPACE IF NOT EXISTS demo_java WITH replication = {'class':'SimpleStrategy', 'replication_factor':1};");
+            session.execute("USE demo_java;");
             session.execute("CREATE TABLE IF NOT EXISTS articles (id UUID PRIMARY KEY, title TEXT, author TEXT);");
             session.execute("INSERT INTO articles (id, title, author) VALUES (uuid(), 'Cassandra with Java', 'Z');");
 
