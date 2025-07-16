4be1a51421eb086732ba5089ee1146736f388fc4
Zartab Nakhwa <zartab@codewithz.com>
2025-07-16 12:01:59 +0530

    PS: First View ID and now

--- pluralsight-casestudy/course_page_views.sql
+++ pluralsight-casestudy/course_page_views.sql
@@ -38,6 +38,51 @@ Select toTimestamp(view_id) from course_page_views where course_id='react-big-pi
 
 Select toTimestamp(view_id) from course_page_views where course_id='react-big-picture'
  and view_id >= minTimeuuid('2025-07-15T10:00:00Z') and view_id <= maxTimeuuid('2025-07-15T11:00:00Z');
- 
+
 truncate course_page_views;
 
+alter table course_page_views add first_view_id timeuuid static;
+
+insert into course_page_views (course_id, view_id, first_view_id) values ('react-big-picture', now(), now()) using ttl 31536000; --
+
+cqlsh:pluralsight> truncate course_page_views;
+cqlsh:pluralsight> alter table course_page_views add first_view_id timeuuid static;
+cqlsh:pluralsight> insert into course_page_views (course_id, view_id, first_view_id)
+               ... values ('react-big-picture', now(), now()) using ttl 31536000;
+cqlsh:pluralsight> select * from course_page_views;
+
+ course_id         | view_id                              | first_view_id
+-------------------+--------------------------------------+--------------------------------------
+ react-big-picture | 33beeca0-620e-11f0-8c27-9f5b4c3f63d7 | 33beecaa-620e-11f0-8c27-9f5b4c3f63d7
+
+(1 rows)
+cqlsh:pluralsight> insert into course_page_views (course_id, view_id, first_view_id) values ('react-big-picture', now(), now()) using ttl 31536000; --
+cqlsh:pluralsight> insert into course_page_views (course_id, view_id, first_view_id) values ('react-big-picture', now(), now()) using ttl 31536000; --
+cqlsh:pluralsight> insert into course_page_views (course_id, view_id, first_view_id) values ('react-big-picture', now(), now()) using ttl 31536000; --
+cqlsh:pluralsight> select * from course_page_views;
+
+ course_id         | view_id                              | first_view_id
+-------------------+--------------------------------------+--------------------------------------
+ react-big-picture | 4b2c8b40-620e-11f0-8c27-9f5b4c3f63d7 | 4b2c8b4a-620e-11f0-8c27-9f5b4c3f63d7
+ react-big-picture | 4839b110-620e-11f0-8c27-9f5b4c3f63d7 | 4b2c8b4a-620e-11f0-8c27-9f5b4c3f63d7
+ react-big-picture | 42a047f0-620e-11f0-8c27-9f5b4c3f63d7 | 4b2c8b4a-620e-11f0-8c27-9f5b4c3f63d7
+ react-big-picture | 33beeca0-620e-11f0-8c27-9f5b4c3f63d7 | 4b2c8b4a-620e-11f0-8c27-9f5b4c3f63d7
+
+(4 rows)
+cqlsh:pluralsight> insert into course_page_views (course_id, view_id, first_view_id) values ('react-big-picture', now(), now()) using ttl 31536000; --
+cqlsh:pluralsight> insert into course_page_views (course_id, view_id, first_view_id) values ('react-big-picture', now(), now()) using ttl 31536000; --
+cqlsh:pluralsight> insert into course_page_views (course_id, view_id, first_view_id) values ('react-big-picture', now(), now()) using ttl 31536000; --
+cqlsh:pluralsight> select * from course_page_views;
+
+ course_id         | view_id                              | first_view_id
+-------------------+--------------------------------------+--------------------------------------
+ react-big-picture | 6c398ae0-620e-11f0-8c27-9f5b4c3f63d7 | 6c398aea-620e-11f0-8c27-9f5b4c3f63d7
+ react-big-picture | 6b72b870-620e-11f0-8c27-9f5b4c3f63d7 | 6c398aea-620e-11f0-8c27-9f5b4c3f63d7
+ react-big-picture | 68831290-620e-11f0-8c27-9f5b4c3f63d7 | 6c398aea-620e-11f0-8c27-9f5b4c3f63d7
+ react-big-picture | 4b2c8b40-620e-11f0-8c27-9f5b4c3f63d7 | 6c398aea-620e-11f0-8c27-9f5b4c3f63d7
+ react-big-picture | 4839b110-620e-11f0-8c27-9f5b4c3f63d7 | 6c398aea-620e-11f0-8c27-9f5b4c3f63d7
+ react-big-picture | 42a047f0-620e-11f0-8c27-9f5b4c3f63d7 | 6c398aea-620e-11f0-8c27-9f5b4c3f63d7
+ react-big-picture | 33beeca0-620e-11f0-8c27-9f5b4c3f63d7 | 6c398aea-620e-11f0-8c27-9f5b4c3f63d7
+
+(7 rows)
+cqlsh:pluralsight>
